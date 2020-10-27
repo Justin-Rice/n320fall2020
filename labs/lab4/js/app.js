146aa7d6ee0fd66534cf3ad2f92f6d47e65f5287
jrice <58012439+Justin-Rice@users.noreply.github.com>
2020-10-26 23:27:27 -0400

    still dont work

--- labs/lab4/js/app.js
+++ labs/lab4/js/app.js
@@ -8,16 +8,7 @@
 //     console.log("pain")
 
 // }
-//tl, tm, tr, cl,c, cr, br, bc, bl,
-// this.tl =  tl;
-// this.tm =  tm;
-// this.tr =  tr;
-// this.cl =  cl;
-// this.c =  c;
-// this.cr =  cr;
-// this.br =  br;
-// this.bm =  bm;
-// this.bl =  bl;
+
 
 let count = 0;;
 
@@ -27,44 +18,6 @@ class Board {
         this.space = space
        
     }
-
-    play(){
-
-
-
-    }
-    
-
-}
-
-class player {
-
-    constructor(name, score, symbol){
-        this.name = name;
-        this.score =score;
-        this.symbol = symbol;
-
-
-    }
-
-    //gives point to the player that won
-    addPoint(){
-        if(p1.checkWin == true){
-        p1.score ++
-        }else{
-            p2.score ++;
-        }
-    }
-
-
-}
-
-class Symbols{
-    constructor(p1, p2){
-        this.p1 = new player("Steve",0, "X");
-        this.p2 = new player("John", 0, 'O')
-    }
-
     // fills the space thats cliked on with the right symbol
     fillSpace(){
         let turn = 0;
@@ -72,55 +25,171 @@ class Symbols{
         for(let x = 0; x < space.length; x++){
             space[x].addEventListener('click',function(event){
               
-                if(event.target.getAttribute("picked") == 1){
+                if(event.target.getAttribute("picked") == 0){
                 if(turn % 2 == 0){
                     
-                event.target.style.fill= "gray";
+                event.target.style.fill= "orange";
                     //changes the attribute number of picked so it cant be clicked again
-                event.target.setAttribute("picked", 0)
+                event.target.setAttribute("picked", "1")
                 turn ++;
                 }else{
 
-                    event.target.style.fill= "black";
+                    event.target.style.fill= "green";
                     //changes the attribute number of picked so it cant be clicked again
-                    event.target.setAttribute("picked", 0)
+                    event.target.setAttribute("picked", "2")
+
 
                     turn ++;
                 }
 
             }
 
-            
              }, false);
         }
         
     }
-        
-//for (var x = 0; x < space.length; x++)
-        
+
+    reset(){
+
+        player.pWon = 0;
+
+        let space = document.getElementsByClassName("SVGSux");
+        let newThing = document.getElementsByTagName("rect");
+
+
+        for(let x = 0; x < space.length; x++){
+            
+              
+                    
+                  newThing[x].style.fill= "white";
+                  newThing[x].setAttribute("picked", "0");
+
+                  
+
     
 
-    //checks to see if a player won 
-    checkWin(){
-        
-
-    }
-    // checks to see what spaces are filled 
-    checkFilled(){
-
     }
 
     
+
+}
+}
+class Player {
+
+    constructor(name, score, symbol){
+        this.name = name;
+        this.score =score;
+        this.symbol = symbol;
+         this.pWon = 0;
+
+    }
+
+     //checks to see if a player won 
+     checkWin() {
+        let space = document.getElementsByClassName("SVGSux");
+        
+         if(this.pWon == 0){
+        if (space[0].getAttribute("picked") == 1 && space[1].getAttribute("picked") == 1 && space[2].getAttribute("picked") == 1 ||
+            space[3].getAttribute("picked") == 1 && space[4].getAttribute("picked") == 1 && space[5].getAttribute("picked") == 1 ||
+            space[6].getAttribute("picked") == 1 && space[7].getAttribute("picked") == 1 && space[8].getAttribute("picked") == 1 ||
+            /*checks for horizontal matches for orange ( X ) */
+            space[0].getAttribute("picked") == 1 && space[3].getAttribute("picked") == 1 && space[6].getAttribute("picked") == 1 ||
+            space[1].getAttribute("picked") == 1 && space[4].getAttribute("picked") == 1 && space[7].getAttribute("picked") == 1 ||
+            space[2].getAttribute("picked") == 1 && space[5].getAttribute("picked") == 1 && space[8].getAttribute("picked") == 1 ||
+            /*checks for vertical matches for orange ( X ) */
+            space[0].getAttribute("picked") == 1 && space[4].getAttribute("picked") == 1 && space[8].getAttribute("picked") == 1 ||
+            space[2].getAttribute("picked") == 1 && space[4].getAttribute("picked") == 1 && space[6].getAttribute("picked") == 1
+            /*checks for diagonal matches for orange ( X ) */
+        ) {
+
+            console.log("nice");
+            this.pWon = 1;
+            return 1;
+
+        }
+
+        if (space[0].getAttribute("picked") == 2 && space[1].getAttribute("picked") == 2 && space[2].getAttribute("picked") == 2 ||
+            space[3].getAttribute("picked") == 2 && space[4].getAttribute("picked") == 2 && space[5].getAttribute("picked") == 2 ||
+            space[6].getAttribute("picked") == 2 && space[7].getAttribute("picked") == 2 && space[8].getAttribute("picked") == 2 ||
+            /*checks for horizontal matches for green ( 0 ) */
+            space[0].getAttribute("picked") == 2 && space[3].getAttribute("picked") == 2 && space[6].getAttribute("picked") == 2 ||
+            space[1].getAttribute("picked") == 2 && space[4].getAttribute("picked") == 2 && space[7].getAttribute("picked") == 2 ||
+            space[2].getAttribute("picked") == 2 && space[5].getAttribute("picked") == 2 && space[8].getAttribute("picked") == 2 ||
+            /*checks for vertical matches for green ( 0 ) */
+            space[0].getAttribute("picked") == 2 && space[4].getAttribute("picked") == 2 && space[8].getAttribute("picked") == 2 ||
+            space[2].getAttribute("picked") == 2 && space[4].getAttribute("picked") == 2 && space[6].getAttribute("picked") == 2
+            /*checks for diagonal matches for green ( O ) */
+        ) {
+
+            console.log("nice2");
+            this.pWon = 1;
+            return 2;
+
+
+
+        }
+
+        }
+    }
+
+
+
+}
+
+class Symbols {
+    constructor(p1, p2){
+        this.p1 = new Player("Steve", 0, "X");
+        this.p2 = new Player("John", 0, 'O');
+    }
+
+    
+        
+//gives point to the player that won
+addPoint(){
+
+    if(this.p1.checkWin() == 1){
+    this.p1.score ++ ;
+
+    document.getElementById("p1score").innerHTML = this.p1.score; 
+    }else if(this.p2.checkWin() == 2){
+        this.p2.score ++;
+        document.getElementById("p2score").innerHTML= this.p2.score; 
+        console.log("stank")
+
+    }
+    console.log(this.p1.checkWin())
+    console.log(this.p2.checkWin())
+
+    
+}
+    
+
+   
 }
 
 
 
 
 let symbol = new Symbols();
-symbol.fillSpace();
+let board = new Board();
+let player = new Player();
+
+board.fillSpace();
 
 
 
+//checks every click to see if a player has won
+let space = document.getElementsByClassName("SVGSux");
+for(let x = 0; x < space.length; x++){
+    space[x].addEventListener('click',function(event){
+      
+                symbol.addPoint();
+           
+     }, false);
+}
 
-
-
+function clickButton(){
+    board.reset();
+    player.pWon = 0;
+    
+    }
