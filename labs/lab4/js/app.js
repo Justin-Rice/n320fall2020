7d91f7320d7f5c34a4c23a83f882f8427175c18b
jrice <58012439+Justin-Rice@users.noreply.github.com>
2020-10-26 23:55:12 -0400

    existance is pain (exccept Julie)

--- labs/lab4/js/app.js
+++ labs/lab4/js/app.js
@@ -10,70 +10,8 @@
 // }
 
 
-let count = 0;;
-
-class Board {
-
-    constructor(space){
-        this.space = space
-       
-    }
-    // fills the space thats cliked on with the right symbol
-    fillSpace(){
-        let turn = 0;
-        let space = document.getElementsByClassName("SVGSux");
-        for(let x = 0; x < space.length; x++){
-            space[x].addEventListener('click',function(event){
-              
-                if(event.target.getAttribute("picked") == 0){
-                if(turn % 2 == 0){
-                    
-                event.target.style.fill= "orange";
-                    //changes the attribute number of picked so it cant be clicked again
-                event.target.setAttribute("picked", "1")
-                turn ++;
-                }else{
-
-                    event.target.style.fill= "green";
-                    //changes the attribute number of picked so it cant be clicked again
-                    event.target.setAttribute("picked", "2")
 
 
-                    turn ++;
-                }
-
-            }
-
-             }, false);
-        }
-        
-    }
-
-    reset(){
-
-        player.pWon = 0;
-
-        let space = document.getElementsByClassName("SVGSux");
-        let newThing = document.getElementsByTagName("rect");
-
-
-        for(let x = 0; x < space.length; x++){
-            
-              
-                    
-                  newThing[x].style.fill= "white";
-                  newThing[x].setAttribute("picked", "0");
-
-                  
-
-    
-
-    }
-
-    
-
-}
-}
 class Player {
 
     constructor(name, score, symbol){
@@ -87,7 +25,7 @@ class Player {
      //checks to see if a player won 
      checkWin() {
         let space = document.getElementsByClassName("SVGSux");
-        
+        console.log(this.pWon);
          if(this.pWon == 0){
         if (space[0].getAttribute("picked") == 1 && space[1].getAttribute("picked") == 1 && space[2].getAttribute("picked") == 1 ||
             space[3].getAttribute("picked") == 1 && space[4].getAttribute("picked") == 1 && space[5].getAttribute("picked") == 1 ||
@@ -136,17 +74,88 @@ class Player {
 
 }
 
+let player = new Player();
+
+class Board {
+
+    constructor(space){
+        this.space = space
+       
+    }
+    // fills the space thats cliked on with the right symbol
+    fillSpace(){
+        let turn = 0;
+        let space = document.getElementsByClassName("SVGSux");
+        for(let x = 0; x < space.length; x++){
+            space[x].addEventListener('click',function(event){
+              
+                if(event.target.getAttribute("picked") == 0){
+                if(turn % 2 == 0){
+                    
+                event.target.style.fill= "orange";
+                    //changes the attribute number of picked so it cant be clicked again
+                event.target.setAttribute("picked", "1")
+                turn ++;
+                }else{
+
+                    event.target.style.fill= "green";
+                    //changes the attribute number of picked so it cant be clicked again
+                    event.target.setAttribute("picked", "2")
+
+
+                    turn ++;
+                }
+
+            }
+
+             });
+        }
+        
+    }
+
+    reset(){
+       // console.log(player.pWon);
+
+        pp1.pWon = 0;
+        pp2.pWon = 0;
+
+
+        let space = document.getElementsByClassName("SVGSux");
+        let newThing = document.getElementsByTagName("rect");
+
+
+        for(let x = 0; x < space.length; x++){
+            
+              
+                    
+                  newThing[x].style.fill= "white";
+                  newThing[x].setAttribute("picked", "0");
+
+                  
+
+    
+
+    }
+
+    
+
+}
+}
+
+let board = new Board();
+let pp1 = new Player("Steve", 0, "X");
+let pp2 = new Player("John", 0, 'O');
 class Symbols {
     constructor(p1, p2){
-        this.p1 = new Player("Steve", 0, "X");
-        this.p2 = new Player("John", 0, 'O');
+        this.p1 = pp1
+        this.p2 = pp2
     }
 
     
         
 //gives point to the player that won
 addPoint(){
-
+    console.log(board.turn)
     if(this.p1.checkWin() == 1){
     this.p1.score ++ ;
 
@@ -171,8 +180,8 @@ addPoint(){
 
 
 let symbol = new Symbols();
-let board = new Board();
-let player = new Player();
+
+
 
 board.fillSpace();
 
@@ -185,7 +194,7 @@ for(let x = 0; x < space.length; x++){
       
                 symbol.addPoint();
            
-     }, false);
+     });
 }
 
 function clickButton(){
